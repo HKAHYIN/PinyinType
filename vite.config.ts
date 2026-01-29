51b580751385eb581a1a4cf2c7e484974aac5204
HKAHYIN <ndbluedino@gmail.com>
2026-01-29 18:25:02 +0800

    added js obfuscator
    although thing is basically open-source in github

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite';
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator';
 
 export default defineConfig({
-  base: '/PinyinType/', 
+  base: '/PinyinType/',
+  plugins: [
+    obfuscatorPlugin({
+      apply: 'build',
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        selfDefending: true,
+        simplify: true,
+        splitStrings: true,
+        stringArray: true,
+        stringArrayEncoding: ['base64'],
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false,
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
-  }
+    sourcemap: false,
+  },
 });
